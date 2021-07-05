1efa09404e6038606a8fc2bfb2ae6ddbc57927e6
maxeemes <max.s.99@yandex.ru>
2021-07-05 23:59:47 +0300

    made Russian symbols check

--- Laba1.cpp
+++ Laba1.cpp
@@ -3,12 +3,6 @@
 #include <string>
 #include <Windows.h>
 /*
-TODO
-*переделать нумерацию символов при шифровке и расшифровке сообщений
-*вынести шифровку и дешифровку в отдельные функции
-
-*/
-/*
 Шифр Вижинера
 */
 
@@ -17,6 +11,7 @@ using namespace std;
 bool AddConsoleTextColor(const string Text, const int Color = 7);//Цвета: 7 - белый(стандартный), 10 - зеленый, 12 - красный, 14 - желтый, 224 - черный на желтом фоне
 int ReadConsoleNum(const string Text = "", const int Color = 7);
 
+int RuSymbolsCount(const string Text);
 string RuVigenereCipherEncrypt(const string Message, const string Key);
 string RuViginereCipherDecrypt(const string EncryptedMessage, const string Key);
 
@@ -37,19 +32,39 @@ int main()
 		case 1: 
 		{
 			AddConsoleTextColor("Введите сообщение...", 14);
-			cin >> Message;
+			//cin >> Message;
+			getchar();
+			getline(cin, Message);
 			AddConsoleTextColor("Ключ для шифра...", 14);
-			cin >> Key;
-			AddConsoleTextColor("Результат: " + RuVigenereCipherEncrypt(Message, Key), 14);
+			//cin >> Key;
+			getline(cin, Key);
+			if (Key.length() == RuSymbolsCount(Key) && Key.length() > 0)
+			{
+				AddConsoleTextColor("Результат: " + RuVigenereCipherEncrypt(Message, Key), 14);
+			}
+			else
+			{
+				AddConsoleTextColor("Введен некорретный ключ! Ключ должен не быть пустым и должен состоять только из символов русского алфавита без буквы ""Ё"" и пробелов!", 12);
+			}
 			break;
 		}
 		case 2:
 		{
 			AddConsoleTextColor("Введите зашифрованное сообщение...", 14);
-			cin >> Message;
+			//cin >> Message;
+			getchar();
+			getline(cin, Message);
 			AddConsoleTextColor("Ключ для шифра...", 14);
-			cin >> Key;
-			AddConsoleTextColor("Результат: " + RuViginereCipherDecrypt(Message, Key), 14);
+			//cin >> Key;
+			getline(cin, Key);
+			if (Key.length() == RuSymbolsCount(Key) && Key.length() > 0)
+			{
+				AddConsoleTextColor("Результат: " + RuViginereCipherDecrypt(Message, Key), 14);
+			}
+			else
+			{
+				AddConsoleTextColor("Введен некорретный ключ! Ключ должен не быть пустым и должен состоять только из символов русского алфавита без буквы ""Ё"" и пробелов!", 12);
+			}
 			break;
 		}
 		case 3:
@@ -94,13 +109,31 @@ int ReadConsoleNum(const string Text, const int Color)
 	return -1;
 }
 
+int RuSymbolsCount(const string Text)
+{
+	int Count = 0;
+	for (int i = 0; i < Text.length(); i++)
+	{
+		Count += tolower(Text[i]) >= -32 && tolower(Text[i]) <= -1;
+	}
+	return Count;
+}
+
 string RuVigenereCipherEncrypt(const string Message, const string Key)
 {
 	int n = 32; //размер алфавита
 	string EncryptedMessage = ""; //переменная для зашифрованного сообщения
-	for (int i = 0; i < Message.length(); i++)//цикл шифра сообщения
+	for (int i = 0, RUi = 0; i < Message.length(); i++)//цикл шифра сообщения
 	{
-		EncryptedMessage += (char)(((tolower((int)Message[i]) + 32 + tolower((int)Key[i % Key.length()]) + 32) % n) - 32);//исправить для символов в конце списка
+		if (tolower(Message[i]) >= -32 && tolower(Message[i]) <= -1)
+		{
+			EncryptedMessage += (char)(((tolower(Message[i]) + 32 + tolower(Key[RUi % Key.length()]) + 32) % n) - 32);
+			RUi++;
+		}
+		else
+		{
+			EncryptedMessage += (char)(tolower(Message[i]));
+		}
 	}
 	return EncryptedMessage;
 }
@@ -109,9 +142,17 @@ string RuViginereCipherDecrypt(const string EncryptedMessage, const string Key)
 {
 	int n = 32; //размер алфавита
 	string UnEncryptedMessage = ""; //переменная для расшифрованного сообщения
-	for (int i = 0; i < EncryptedMessage.length(); i++)
+	for (int i = 0, RUi = 0; i < EncryptedMessage.length(); i++)
 	{
-		UnEncryptedMessage += (char)(((tolower(EncryptedMessage[i]) + 32 - n - tolower((int)Key[i % Key.length()]) + 32) % n) - 32);
+		if (tolower(EncryptedMessage[i]) >= -32 && tolower(EncryptedMessage[i]) <= -1)
+		{
+			UnEncryptedMessage += (char)(((tolower(EncryptedMessage[i]) + 32 - n - tolower(Key[RUi % Key.length()]) + 32) % n) - 32);
+			RUi++;
+		}
+		else
+		{
+			UnEncryptedMessage += (char)(tolower(EncryptedMessage[i]));
+		}
 	}
 
 	return UnEncryptedMessage;
